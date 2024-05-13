ba38f635287d821422362608d69fa043a78ea9d9
김유진 <gracecancho@gmail.com>
2024-05-13 09:24:23 +0900

    스프링 메시지 소스 설정 및 사용

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,12 +2,25 @@ package hello.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class ItemServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ItemServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServiceApplication.class, args);
+    }
+
+//	스프링 메시지 소스 설정 직접 등록 예시
+//	@Bean
+//	public MessageSource messageSource() {
+//		ResourceBundleMessageSource messageSource = new
+//				ResourceBundleMessageSource();
+//		messageSource.setBasenames("messages", "errors");
+//		messageSource.setDefaultEncoding("utf-8");
+//		return messageSource;
+//	}
 
 }
